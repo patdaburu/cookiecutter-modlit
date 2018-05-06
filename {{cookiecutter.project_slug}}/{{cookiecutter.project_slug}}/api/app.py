66360fd15829b7845b6a45aeb4d5edc124a7531c
patdaburu <pat@daburu.net>
2018-05-06 13:51:25 -0500

    Added some routes.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/api/app.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/api/app.py
@@ -6,9 +6,15 @@
 .. moduleauthor:: {{cookiecutter.author_name}} <{{cookiecutter.author_email}}>
 
 :var app: the Flask application object
-:type app: :py:class:`modlit.api.Flask`
+:type app: :py:class:`flask.Flask`
 """
 # pylint: disable=invalid-name
+# pylint: disable=unused-import
 from modlit.api import ModlitFlask
 
 app: ModlitFlask = ModlitFlask(__name__.split('.')[0])  #: the Flask app itself
+
+# Import the routes modules.  (This may look unconventional, but it is in-line
+# with Flask's design.  Go to http://flask.pocoo.org/docs/1.0/patterns/packages/
+# and read the section on "Circular Imports" for more information.)
+import {{cookiecutter.project_slug}}.api.routes
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/api/routes.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/api/routes.py
@@ -2,9 +2,54 @@
 # -*- coding: utf-8 -*-
 
 """
-.. currentmodule:: {{cookiecutter.project_slug}}.routes
+.. currentmodule:: {{cookiecutter.project_slug}}.api.routes
 .. moduleauthor:: {{cookiecutter.author_name}} <{{cookiecutter.author_email}}>
 
 This module contains the Flask app definition for the project.
 """
-from {{cookiecutter.project_slug}}.api import app
+from flask import jsonify
+from geoalchemy2 import functions
+import {{cookiecutter.project_slug}} as package
+from .app import app
+from ..model.places import Place
+
+
+# https://www.youtube.com/watch?v=WxGBoY5iNXY&t=817s
+
+
+@app.route('/info', methods=['GET'])
+def get_info():
+    """
+    Get general information about the running API.
+
+    :return: information about the running API
+    """
+    return jsonify({
+        'package': package.__name__,
+        'version': package.__version__,
+        'release': package.__release__
+    })
+
+
+@app.route('/places', methods=['GET'])
+def get_places():
+    """
+
+    :return:
+    """
+    session = app.session()
+    places = session.query(
+        Place.id,
+        Place.label,
+        functions.ST_AsGeoJSON(Place.geometry).label('geometry')
+    )
+    output = []
+    for place in places:
+        place_data = {
+            'id': place.id,
+            'label': place.label,
+            'geometry': place.geometry
+        }
+        output.append(place_data)
+    session.close()
+    return jsonify({'places': output})
