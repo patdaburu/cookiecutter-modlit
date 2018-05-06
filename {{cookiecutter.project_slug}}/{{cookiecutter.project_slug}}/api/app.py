5e9c7ba3ce920ac2ee4829ccb7652caea3737474
patdaburu <pat@daburu.net>
2018-05-06 10:26:57 -0500

    Refactoring API app into modlit.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/api/app.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/api/app.py
@@ -6,33 +6,9 @@
 .. moduleauthor:: {{cookiecutter.author_name}} <{{cookiecutter.author_email}}>
 
 :var app: the Flask application object
-:type app: :py:class:`flask.Flask`
-:var engine: the SQLAlchemy engine
-:type engine: :py:class:`sqlalchemy.engine.base.Engine`
+:type app: :py:class:`modlit.api.Flask`
 """
 # pylint: disable=invalid-name
-from flask import Flask
-from sqlalchemy.engine.base import Engine
-from typing import Any, Dict
+from modlit.api import ModlitFlask
 
-__shared__: Dict[str, Any] = {}  #: a dictionary of shared values
-
-app: Flask = Flask('{{cookiecutter.project_slug}}')
-
-
-def get_engine() -> Engine:
-    """
-    Get the SQLAlchemy engine for the application.
-
-    :return: the SQLAlchemy engine
-    """
-    return __shared__['__engine__']
-
-
-def install_engine(engine: Engine):
-    """
-    Install the SQLAlchemy engine for the application.
-
-    :param engine: the engine
-    """
-    __shared__['__engine__'] = engine
+app: ModlitFlask = ModlitFlask(__name__.split('.')[0])  #: the Flask app itself
